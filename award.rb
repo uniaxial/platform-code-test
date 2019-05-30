f7b792d0c444d56841df8e77a99636e6eb4c33e9
J. Lunn <encode@engineer.com>
2019-05-29 22:27:18 -0500

    Attempt to refactor conditionals away in award.rb with strategy pattern

--- award.rb
+++ award.rb
@@ -1,33 +1,62 @@
 class Award
-    attr_reader :name, :expires_in, :quality
 
-    def initialize(name, expires_in, quality)
-        @name = name
-        @expires_in = expires_in
-        @quality = quality
-    end
+  attr_reader :name
+  attr_accessor :expires_in, :quality
+ 
+  def initialize(name, expires_in, quality)
+    @name = name
+    @expires_in = expires_in
+    @quality = quality
 
-    def update
-        case name
-        when 'Blue Compare'
-            @quality += 1 if @quality < 50
-            @quality += 1 if @expires_in < 11 && @quality < 50
-            @quality += 1 if @expires_in < 6 && @quality < 50
-            @expires_in -= 1
-            @quality = @quality - @quality if @expires_in < 0
-        when 'Blue Distinction Plus'
-        when 'Blue First'
-            @quality += 1 if @quality < 50
-            @expires_in -= 1
-            @quality += 1 if @expires_in < 0 && @quality < 50
-        when 'Blue Star'
-            @quality -= 2 if @expires_in > 0 && @quality.between?(1,50)
-            @quality -= 4 if (@expires_in == 0 || @expires_in < 0) && @quality.between?(4,50)
-            @expires_in -= 1
-        else
-            @quality -= 1 if @expires_in > 0 && @quality.between?(1,50)
-            @quality -= 2 if (@expires_in == 0 || @expires_in < 0) && @quality < 50
-            @expires_in -= 1
-        end
-    end
+    @strategies = {}
+    @strategies['Blue Compare'] = BlueCompareAward
+    @strategies['Blue Distinction Plus'] = BlueDistinctionPlusAward
+    @strategies['Blue First'] = BlueFirstAward
+    @strategies['Blue Star'] = BlueStarAward
+    @strategies['NORMAL ITEM'] = NormalItemAward
+  end
+
+  def update
+    strategy = @strategies[@name]
+    strategy.update(self)
+  end
+end
+
+class BlueCompareAward
+  def self.update(context)
+    context.quality += 1 if context.quality < 50
+    context.quality += 1 if context.expires_in < 11 && context.quality < 50
+    context.quality += 1 if context.expires_in < 6 && context.quality < 50
+    context.expires_in -= 1
+    context.quality = context.quality - context.quality if context.expires_in < 0
+  end
+end
+
+class BlueDistinctionPlusAward
+  def self.update(context)
+  end
+end
+
+class BlueFirstAward
+  def self.update(context)
+    context.quality += 1 if context.quality < 50
+    context.expires_in -= 1
+    context.quality += 1 if context.expires_in < 0 && context.quality < 50
+  end
+end
+
+class BlueStarAward
+  def self.update(context)
+    context.quality -= 2 if context.expires_in > 0 && context.quality.between?(1,50)
+    context.quality -= 4 if (context.expires_in == 0 || context.expires_in < 0) && context.quality.between?(4,50)        
+    context.expires_in -= 1
+  end
+end
+
+class NormalItemAward
+  def self.update(context)
+    context.quality -= 1 if context.expires_in > 0 && context.quality.between?(1,50)
+    context.quality -= 2 if (context.expires_in == 0 || context.expires_in < 0) && context.quality < 50
+    context.expires_in -= 1
+  end
 end
