23b778841fead6e2eb2a990648a9710b196d93fa
J. Lunn <encode@engineer.com>
2019-05-29 22:13:35 -0500

    Refactor conditionals from update_quality.rb to award.rb

--- award.rb
+++ award.rb
@@ -1,1 +1,33 @@
-Award = Struct.new(:name, :expires_in, :quality)
+class Award
+    attr_reader :name, :expires_in, :quality
+
+    def initialize(name, expires_in, quality)
+        @name = name
+        @expires_in = expires_in
+        @quality = quality
+    end
+
+    def update
+        case name
+        when 'Blue Compare'
+            @quality += 1 if @quality < 50
+            @quality += 1 if @expires_in < 11 && @quality < 50
+            @quality += 1 if @expires_in < 6 && @quality < 50
+            @expires_in -= 1
+            @quality = @quality - @quality if @expires_in < 0
+        when 'Blue Distinction Plus'
+        when 'Blue First'
+            @quality += 1 if @quality < 50
+            @expires_in -= 1
+            @quality += 1 if @expires_in < 0 && @quality < 50
+        when 'Blue Star'
+            @quality -= 2 if @expires_in > 0 && @quality.between?(1,50)
+            @quality -= 4 if (@expires_in == 0 || @expires_in < 0) && @quality.between?(4,50)
+            @expires_in -= 1
+        else
+            @quality -= 1 if @expires_in > 0 && @quality.between?(1,50)
+            @quality -= 2 if (@expires_in == 0 || @expires_in < 0) && @quality < 50
+            @expires_in -= 1
+        end
+    end
+end
--- update_quality.rb
+++ update_quality.rb
@@ -2,26 +2,6 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    case award.name
-      when 'Blue Compare'
-        award.quality += 1 if award.quality < 50
-        award.quality += 1 if award.expires_in < 11 && award.quality < 50
-        award.quality += 1 if award.expires_in < 6 && award.quality < 50
-        award.expires_in -= 1
-        award.quality = award.quality - award.quality if award.expires_in < 0
-      when 'Blue Distinction Plus'
-      when 'Blue First'
-        award.quality += 1 if award.quality < 50
-        award.expires_in -= 1
-        award.quality += 1 if award.expires_in < 0 && award.quality < 50
-      when 'Blue Star'
-        award.quality -= 2 if award.expires_in > 0 && award.quality.between?(1,50)
-        award.quality -= 4 if (award.expires_in == 0 || award.expires_in < 0) && award.quality.between?(4,50)        
-        award.expires_in -= 1
-      else
-        award.quality -= 1 if award.expires_in > 0 && award.quality.between?(1,50)
-        award.quality -= 2 if (award.expires_in == 0 || award.expires_in < 0) && award.quality < 50
-        award.expires_in -= 1
-    end
+    award.update
   end
 end
