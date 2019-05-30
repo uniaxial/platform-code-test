e95926a9dd90bdf2dea494e5e817b7b76cc5d768
J. Lunn <encode@engineer.com>
2019-05-29 22:00:03 -0500

    Initial commit

--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,26 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
+    case award.name
+      when 'Blue Compare'
+        award.quality += 1 if award.quality < 50
+        award.quality += 1 if award.expires_in < 11 && award.quality < 50
+        award.quality += 1 if award.expires_in < 6 && award.quality < 50
+        award.expires_in -= 1
+        award.quality = award.quality - award.quality if award.expires_in < 0
+      when 'Blue Distinction Plus'
+      when 'Blue First'
+        award.quality += 1 if award.quality < 50
+        award.expires_in -= 1
+        award.quality += 1 if award.expires_in < 0 && award.quality < 50
+      when 'Blue Star'
+        award.quality -= 2 if award.expires_in > 0 && award.quality.between?(1,50)
+        award.quality -= 4 if (award.expires_in == 0 || award.expires_in < 0) && award.quality.between?(4,50)        
+        award.expires_in -= 1
       else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
+        award.quality -= 1 if award.expires_in > 0 && award.quality.between?(1,50)
+        award.quality -= 2 if (award.expires_in == 0 || award.expires_in < 0) && award.quality < 50
+        award.expires_in -= 1
     end
   end
 end
